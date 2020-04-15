68eaa528fd068c20ab599c689d6cd13ba29d4da9
Q Lee <qliangw@outlook.com>
2020-04-15 16:37:30 +0800

    test

---
+++ string/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    cout << "hello, " << name << endl;
+    cout << name.size() << endl;
+    name = name + name;
+    cout << name << endl;
+    string a = "C";
+    string b = "++";
+    auto c = a + b;
+    cout << c << endl;
+
+    system("pause");
+    return 0;
+}
