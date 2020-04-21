91aa42a5129507759c53a125700798cb39334bbc
Q Lee <qliangw@outlook.com>
2020-04-21 16:27:50 +0800

    集合

--- set/set_example.cpp
+++ set/set_example.cpp
@@ -6,20 +6,29 @@ using namespace std;
 void set_example()
 {
     set<int> S = {3, 2, 1, 4, 5 };
-
-    S.insert(1);
+    
+    //插入
+    S.insert(0);
     S.insert(7);
 
+    //查找与删除
     auto iter = S.find(0);
-    iter = S.find(5);
-
     if(iter != S.end())
         S.erase(iter);
+    iter = S.find(5);
+    if(iter != S.end())
+        S.erase(iter);
+    S.erase(5);
 
+    //迭代器位置
     iter = S.begin();
-    ++iter;//对于迭代器来说 ++放在后面效率会低一些
-    --iter;
+    cout << *iter << endl;
+     ++iter;//对于迭代器来说 ++放在后面效率会低一些
+     cout << *iter << endl;
+     --iter;
+     cout << *iter << endl;
 
+    //遍历集合
     for(auto iter = S.cbegin(); iter != S.end(); ++iter)
         cout << *iter << " ";   
     cout << endl;
