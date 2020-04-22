2511541d5cb1f0b747ccc727bc1bd956423bad9f
Q Lee <qliangw@outlook.com>
2020-04-22 09:25:02 +0800

    次序统计量

--- set/order_statistics_set.cpp
+++ set/order_statistics_set.cpp
@@ -13,7 +13,7 @@ void order_statistics_set()
     int k;
     cout << "input k:";
     cin >> k;
-    // 第一种方法:
+    // 第一种方法: 优先选择
     if(k <= 0 || k > S.size())
         cout << "err" << endl;
     else
@@ -26,6 +26,19 @@ void order_statistics_set()
     }
     
 
-    //第二种方法
-    
+    //第二种方法 较慢
+    vector<int> V;
+    //预留k-1个元素空间
+    V.reserve(k-1);
+    //删除S中的前k-1个元素并暂存到V中
+    for(int i = 0; i < k-1; ++i)
+    {
+        V.push_back(*S.begin());
+        S.erase(S.begin());
+    }
+    cout << *S.begin() << endl;
+    //将前k-1个元素放回S中，也可以对V使用基于范围的for循环
+    S.insert(V.begin(), V.end());
+    for(size_t i = 0; i < V.size(); ++i)
+        S.insert(V[i]);
 }
