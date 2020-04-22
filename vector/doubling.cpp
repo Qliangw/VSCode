c320183a625a0a0b9d6f288e5929ed203a628953
Q Lee <qliangw@outlook.com>
2020-04-22 16:45:17 +0800

    容量

---
+++ vector/doubling.cpp
@@ -0,0 +1,25 @@
+#include "myVector.h"
+
+void doubling()
+{
+    const size_t n = 40;
+    // size()返回当前元素个数, capacity()返回当前容量.
+
+    vector<int> A;
+    for (size_t i = 0; i < n; ++i)
+    {
+        cout << A.size() << " " << A.capacity() << endl;
+        A.push_back(0); 
+    }
+    cout << A.size() << " " << A.capacity() << endl;
+
+    vector<int> B;
+    // 提前预留容量n.
+    B.reserve(n);
+    for (size_t i = 0; i < n; ++i)
+    {
+        cout << B.size() << " " << B.capacity() << endl;
+        B.push_back(0);
+    }
+    cout << B.size() << " " << B.capacity() << endl;    
+}
--- vector/myVector.h
+++ vector/myVector.h
@@ -53,5 +53,6 @@ void print_all(const T& S)
 int test_binary_vector();
 void sortedVector_and_unsortedVector();
 void vector_example();
+void doubling();
 
 #endif
