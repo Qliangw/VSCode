eb2ad960155f9b560b65b664d57323f2915bab67
Jason_Smith <qili00001@gmail.com>
2020-04-23 20:48:22 +0800

    slist.cpp

---
+++ forward_list/myForwardList.h
@@ -0,0 +1,9 @@
+#ifndef _MYFORWARDLIST_H
+#define _MYFORWARDLIST_H
+
+#include <iostream>
+#include <vector>
+
+void slist();
+
+#endif //  _MYFORWARDLIST_H
---
+++ forward_list/slist.cpp
@@ -0,0 +1,49 @@
+#include "myForwardList.h"
+
+using namespace std;
+
+struct snode{
+    int data;
+    snode* next;
+};
+
+void slist()
+{
+    snode sentinal;
+    snode* header = &sentinal;
+    snode A = {1, NULL};
+    snode B = {2, NULL};
+    snode C = {3, NULL};
+    snode* p;
+    p = header;
+    p->next = &A;
+    p = p->next;
+    p->next = &B;
+    p = p->next;
+    p->next = &C;
+    for(p = header->next; p != NULL; p = p->next)
+        cout << p->data << endl;
+
+    //先将节点A之后的节点B删除
+    p = &A;
+    snode* q = p->next;
+    p->next = q->next;
+    //再将刚删除的节点B插入到节点C之后
+    p = p->next;
+    q->next = p->next;
+    p->next = q;
+    for (p = header->next; p!= NULL; p = p->next)
+    {
+        cout << p->data << endl;
+    }
+    vector<snode> V = {{}, {1, NULL}, {2, NULL}, {3, NULL}};
+    header = p = &V[0];
+    for (size_t i = 1; i < V.size(); ++i)
+    {
+        p->next = &V[i];
+        p = p->next;
+    }
+    for (p = header->next; p != NULL; p = p->next)
+        cout << p->data << endl;
+    
+}
