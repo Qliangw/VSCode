39c347065f2f6a628132096aff144f342a9c18b1
Q Lee <qliangw@outlook.com>
2020-04-24 15:15:33 +0800

    链表的增删

---
+++ list/insert_erase_splice_find.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include "myList.h"
+
+using namespace std;
+
+void insert_erase_splice_find()
+{
+    list<int> L = {-2, -1, 1, 2};
+    auto iter = L.begin();
+    while(iter != L.end() && *iter < 0)
+        ++iter;
+    L.insert(iter, 0);
+    cout << *iter << endl;
+    iter = L.begin();
+    while(iter != L.end())
+        if(*iter > 0)
+            iter = L.erase(iter);
+        else
+            ++iter;
+    
+    list<int> R = {-5, -4, -3};
+    L.splice(L.begin(), R, ++R.begin(), R.end());
+    for(const auto & x : L)
+        cout << x << endl;
+    iter = find(L.begin(), L.end(), 0);
+    if(iter != L.end())
+        L.erase(iter);
+        
+}
---
+++ list/myList.h
@@ -0,0 +1,7 @@
+#ifndef _MYLIST_H
+#define _MYLIST_H
+
+void list_example();
+void insert_erase_splice_find();
+
+#endif
